dbd2ed4c07f876781147694f58db2ce74bf00b36
Pedro Ghiotti <pedro.ghiotti.m@gmail.com>
2025-03-06 16:43:13 -0300

    feat: adicionada autorização com base em role de usuário para acessar endpoints especificos.

--- src/main/java/pg/decola_tech_avanade_2025/cursos/spring_security/security/WebSecurityConfiguration.java
+++ src/main/java/pg/decola_tech_avanade_2025/cursos/spring_security/security/WebSecurityConfiguration.java
@@ -6,11 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /*
     Como as classes apresentadas no curso estavam dpreciadas e, algumas,
@@ -19,6 +23,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
     Podem ser encontrados nos seguintes links:
         https://docs.spring.io/spring-security/reference/servlet/getting-started.html
         https://docs.spring.io/spring-security/reference/servlet/authorization/method-security.html
+        https://docs.spring.io/spring-security/reference/servlet/configuration/java.html#_securityfilterchain_endpoints
+        https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/basic.html
 
     * '{noop}' não faz parte das senhas; É um prefixo que define a estratégia de criptografia
     a ser utilizada.
@@ -29,7 +35,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableMethodSecurity
 public class WebSecurityConfiguration {
     @Bean
-    @ConditionalOnMissingBean(UserDetailsService.class)
     InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         return new InMemoryUserDetailsManager(
             User.withUsername("pedroghiotti")
@@ -44,7 +49,32 @@ public class WebSecurityConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean(AuthenticationEventPublisher.class)
+    public SecurityFilterChain userSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/user/**")
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().hasRole("USER")
+        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/admin/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().hasRole("ADMIN")
+                ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+            .anyRequest().permitAll()
+        );
+        return http.build();
+    }
+
+    @Bean
     DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher delegate) {
         return new DefaultAuthenticationEventPublisher(delegate);
     }
